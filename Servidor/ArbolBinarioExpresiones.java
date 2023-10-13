029a195389037a988178fbba394aae104b588049
EdwardAbAr <142754872+EdwardAbAr@users.noreply.github.com>
2023-10-12 18:51:53 -0600

    Add files via upload

---
+++ Servidor/ArbolBinarioExpresiones.java
@@ -0,0 +1,38 @@
+package Servidor;
+
+public class ArbolBinarioExpresiones {
+    NodoArbol raiz;
+
+    public static void main(){
+        new UsaArbolBinarioExpresiones().setVisible(true);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Servidor/PilaArbolExp.java
@@ -0,0 +1,33 @@
+package Servidor;
+
+public class PilaArbolExp {
+    private NodoPila tope;
+
+    public PilaArbolExp(){
+        tope = null;
+    }
+
+    public void insertar(NodoArbol elemento){
+        NodoPila nuevo;
+        nuevo = new NodoPila(elemento);
+        tope = nuevo;
+    }
+    public boolean pilaVacia(){
+        return tope == null;
+    }
+    public NodoArbol topePila(){
+        return tope.dato;
+    }
+
+    public void ReiniciarPila(){
+        tope = null;
+    }
+    public NodoArbol quitar(){
+        NodoArbol aux = null;
+        if (!pilaVacia()){
+            aux = tope.dato;
+            tope = tope.siguiente;
+        }
+        return aux;
+    }
+}
