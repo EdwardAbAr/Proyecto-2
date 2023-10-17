9629aa0639e9999b9421eed75b15d242475e64c7
diegosalasov <142754889+diegosalasov@users.noreply.github.com>
2023-10-16 22:04:18 -0600

    Add files via upload

---
+++ Proyecto2-DatosI/src/Arbol/NodoPila.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Arbol;
+
+/**
+ *
+ * @author Usuario
+ */
+public class NodoPila {
+    NodoArbol dato;
+    NodoPila siguiente;
+    
+    public NodoPila(NodoArbol x){
+    
+        dato = x;
+        siguiente = null;
+    }
+    
+}
